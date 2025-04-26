9712f4171c05a38ad2cd747ceb126c75e3e1711a
Samet Arslan <sametarslan_ce@icloud.com>
2025-04-26 03:18:54 +0300

    google search verify with search console

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,16 @@
 import "@/styles/globals.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="M6RfthUGQt6kLh85ghLE1-EJfShW_uscwD7V-dn-umc"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
