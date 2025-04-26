daaa1a761644afc2393b2b5d4dd9e00a1a9b4cb8
Samet Arslan <sametarslan_ce@icloud.com>
2025-04-26 03:24:49 +0300

    sitemap.xml.js file added

---
+++ pages/sitemap.xml.js
@@ -0,0 +1,39 @@
+import { getAllHaberler } from "@/lib/api";
+
+export async function getServerSideProps({ res }) {
+  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000";
+
+  let haberler = [];
+  try {
+    const response = await getAllHaberler();
+    haberler = response.results || [];
+  } catch (error) {
+    console.error("Sitemap API fetch error:", error);
+  }
+
+  const staticUrls = [
+    `<url><loc>${baseUrl}/</loc><priority>1.0</priority></url>`,
+  ];
+
+  const haberUrls = haberler.map((haber) => {
+    return `<url><loc>${baseUrl}/haber/${haber.id}</loc><priority>0.8</priority></url>`;
+  });
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${staticUrls.join("\n")}
+${haberUrls.join("\n")}
+</urlset>`;
+
+  res.setHeader("Content-Type", "text/xml");
+  res.write(sitemap);
+  res.end();
+
+  return {
+    props: {},
+  };
+}
+
+export default function Sitemap() {
+  return null;
+}
